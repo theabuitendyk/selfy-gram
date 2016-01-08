daebb5df97b011141f2829a705e003b17c1850d6
Thea Buitendyk <theabuitendyk@gmail.com>
2016-01-07 18:43:51 -0800

    Initial Commit

---
+++ Week1-Day2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week1-Day2
+//
+//  Created by Phoenix on 2016-01-07.
+//  Copyright © 2016 Phoenix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Week1-Day2Tests/Week1_Day2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Week1_Day2Tests.swift
+//  Week1-Day2Tests
+//
+//  Created by Phoenix on 2016-01-07.
+//  Copyright © 2016 Phoenix. All rights reserved.
+//
+
+import XCTest
+@testable import Week1_Day2
+
+class Week1_Day2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
